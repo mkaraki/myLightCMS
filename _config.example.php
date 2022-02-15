ba3b9886ba2cf363b3d81ebd6453c327e947e069
mkaraki <26180919+mkaraki@users.noreply.github.com>
2022-02-15 22:25:29 +0900

    Add new features - Date sort - Site description - Site admin view

--- _config.example.php
+++ _config.example.php
@@ -1,2 +1,5 @@
 <?php
 $blog_title = "My Personal Blog";
+$blog_desc = "This is my personal blog.";
+
+$owner = 'myLightCMS Developper Team &lt;<a href="mailto:mylightcms@example.com">mylightcms@example.com</a>&gt;';
